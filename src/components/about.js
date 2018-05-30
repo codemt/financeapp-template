757a6fb98dfdf91fe00a183f4778008b823687ce
Mithilesh Tarkar <mithilesh.tarkar@ves.ac.in>
2018-05-30 18:41:09 +0530

    checking issues with react router and react materialize

--- src/components/about.js
+++ src/components/about.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
  class About extends Component {
   render() {
     return (
-        <div class="row">
-    <div class="col s12 m6">
-      <div class="card">
-        <div class="card-image">
+        <div className="row">
+    <div className="col s12 m6">
+      <div className="card">
+        <div className="card-image">
           <img src="http://businesscentrictechnology.com/wp-content/uploads/2017/11/BCT-%E2%80%93-Nov-3-%E2%80%93-Top-3-Team-Morale-Killers-to-Avoid.jpg" />
-          <span class="card-title">Our Team</span>
+          <span className="card-title">Our Team</span>
           
         </div>
-        <div class="card-content">
+        <div className="card-content">
           <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
         </div>
       </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,10 +4,12 @@ import {  NavLink } from 'react-router-dom';
  class NavBar extends Component {
   render() {
     return (
+        <div>
         <Navbar brand='Finance App'right style={{backgroundColor:"#2c3e50"}}>
-        <NavItem href="/about" to="/about">About us </NavItem>
-        <NavItem href="/team" to="/team">Our Professionals</NavItem>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/team">Team</NavLink></li>
       </Navbar>
+      </div>
     )
   }
 }
